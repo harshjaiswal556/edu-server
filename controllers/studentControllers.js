d870a1a01a06b4d74357c2259f9d072d91f8de52
harshjaiswal556 <tech.harsh2003@gmail.com>
2025-02-21 14:36:20 +0530

    hashing

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -1,5 +1,4 @@
 const StudentModel = require("../models/studentModel");
-const User = require("../models/usersModel");
 
 module.exports.getStudents = async (req, res) => {
   const start = parseInt(req.query._start) || 0;
@@ -9,15 +8,3 @@ module.exports.getStudents = async (req, res) => {
   res.send(data);
 };
 
-module.exports.getLoggedInUser = async(req, res)=>{
-  const {username, password} = req.body;
-  try {
-    const user = await User.findOne({username});
-    if(user && (user.password === password)){
-      return res.status(200).json({ message: 'Login successful', user });
-    }
-    return res.status(400).json({ message: 'Invalid credentials' });
-  } catch (error) {
-    return res.status(500).json({ message: 'Server error', error });
-  }
-}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const { getLoggedInUser } = require('../controllers/studentControllers');
+const { getLoggedInUser, createUser } = require('../controllers/userControllers');
 
 const router = Router();
 
 router.get('/users', getLoggedInUser);
+router.post('/create-user', createUser);
 
 module.exports = router;
