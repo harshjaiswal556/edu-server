a65f5ac9576bebb61bdc4d845403a0850d1cd59b
harshjaiswal556 <tech.harsh2003@gmail.com>
2025-02-25 11:45:08 +0530

    update student by id

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,10 +1,11 @@
     const {Router} = require('express');
-    const { getStudents, createStudent, getStudentById } = require('../controllers/studentControllers');
+    const { getStudents, createStudent, getStudentById, updateStudentById } = require('../controllers/studentControllers');
 
     const router = Router();
 
     router.get('/students', getStudents);
     router.get('/students/:id', getStudentById);
     router.post('/create-student', createStudent);
+    router.put('/students/:_id', updateStudentById);
 
     module.exports = router;
