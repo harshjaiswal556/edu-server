0e578aa3a1f64fdc6ad2fae946c0794329d99919
sreejith ks <sreejithksgupta2255@gmail.com>
2025-02-27 10:38:28 +0530

    added getnotifications

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -14,3 +14,13 @@ module.exports.sendNotification = async(req, res)=>{
         return res.json(error);
     }
 }
+
+module.exports.getNotifications = async (req, res) => {
+  const start = parseInt(req.query._start) || 0;
+  const limit = parseInt(req.query._limit) || 10;
+
+  const data = await Notification.find()
+    .skip(start)
+    .limit(limit - start);
+  res.send(data);
+};
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,8 +1,11 @@
 const {Router} = require('express');
-const { sendNotification } = require('../controllers/notificationControllers');
+const { sendNotification ,getNotifications} = require('../controllers/notificationControllers');
+const { get } = require('mongoose');
 
 const router = Router();
 
 router.post('/send-notification', sendNotification);
+router.get('/notifications', getNotifications);
+
 
 module.exports = router;
