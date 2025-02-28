b16dfc073b782cf5509c871c274a27af2288a973
harshjaiswal556 <tech.harsh2003@gmail.com>
2025-02-28 12:19:42 +0530

    notification route Api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,11 @@
 const {Router} = require('express');
 const { getLoggedInUser, createUser, allUsers } = require('../controllers/userControllers');
-const { readNotifications } = require('../controllers/notificationControllers');
 
 const router = Router();
 
 router.post('/users', getLoggedInUser);
 router.get('/all-users', allUsers);
 router.post('/create-user', createUser);
-router.put('/read-notification/:_id', readNotifications)
+
 
 module.exports = router;
